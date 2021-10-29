52d6252f1f3213be6e596c18c2714f0889cc9951
vanminh <vanminh.dev@gmail.com>
2021-10-29 20:41:41 +0700

    fix: fix oktaAuth import issue

--- apps/social-network-frontend/src/app/AppWithRouterAccess.tsx
+++ apps/social-network-frontend/src/app/AppWithRouterAccess.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { LoginCallback, Security } from '@okta/okta-react';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
-import { Login, oktaAuth } from '@sn-htc/social-network-frontend/feature-auth';
-import { oktaSignInConfig } from '@sn-htc/social-network-frontend/config-constants';
+import { Login } from '@sn-htc/social-network-frontend/feature-auth';
+import { oktaAuth, oktaSignInConfig } from '@sn-htc/social-network-frontend/config-constants';
 import PrivateRoutesContainer from './PrivateRoutesContainer';
 import { Home } from '@sn-htc/social-network-frontend/feature-home';
 
