604df632bd1b372478f7617a2324c11f85405c2a
Wizard2069 <wizard201366@gmail.com>
2021-11-02 20:29:03 +0700

    test: change sub claim of jwt test

--- libs/sn-application/sn-common/src/testFixtures/java/com/htcompany/sncommon/test/util/JwtUtil.java
+++ libs/sn-application/sn-common/src/testFixtures/java/com/htcompany/sncommon/test/util/JwtUtil.java
@@ -11,7 +11,7 @@ public class JwtUtil {
 
     public static Mono<Jwt> createReactiveJwt(String tokenValue, List<String> authorities) {
         Map<String, Object> claims = new HashMap<>();
-        claims.put("sub", "admin");
+        claims.put("sub", "12345");
         claims.put("groups", authorities);
 
         return Mono.just(new Jwt(
