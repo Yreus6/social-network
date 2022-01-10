bf64fff7db552b2dfb7963c04dc7912739adbf0c
Wizard2069 <wizard201366@gmail.com>
2022-01-10 18:20:35 +0700

    ci: fix warning test and add debug option to investigate

--- libs/social-network-frontend/components-routes/src/lib/PrivateRoute.tsx
+++ libs/social-network-frontend/components-routes/src/lib/PrivateRoute.tsx
@@ -22,6 +22,10 @@ export const PrivateRoute = ({ hasAnyAuthorities = [], children, ...rest }: Priv
         setAuthorities(userAuthorities);
       });
     }
+
+    return () => {
+      setAuthorities(null);
+    };
   }, [authState, oktaAuth]);
 
   return (
