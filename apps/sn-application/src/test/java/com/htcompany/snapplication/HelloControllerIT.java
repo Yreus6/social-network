201c66ba977eb79c7ffaa6d505599abecab29af3
Wizard2069 <wizard201366@gmail.com>
2023-05-27 23:55:02 +0700

    chore: fix lint and add cypress config

--- apps/sn-application/src/test/java/com/htcompany/snapplication/HelloControllerIT.java
+++ apps/sn-application/src/test/java/com/htcompany/snapplication/HelloControllerIT.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class HelloControllerTests {
+class HelloControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
